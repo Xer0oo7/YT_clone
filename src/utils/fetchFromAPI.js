dcf2f3c8c928b0ec0b569d6f4100c55e224ffbb8
Deva Nandan <devanandanmr2006@gmail.com>
2025-03-25 18:23:57 +0530

    Add files via upload

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,24 @@
+// fetchFromAPI.js
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key':"310a752742msh3f4175a84af1db4p18dc1djsn252089815ae7",
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  try {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+  } catch (error) {
+    console.error('API Error:', error.response ? error.response.data : error.message);
+    throw error;
+  }
+};
