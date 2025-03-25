96c21d8e3d717af946d03601ee2dfd94b4581f8e
Deva Nandan <devanandanmr2006@gmail.com>
2025-03-25 20:07:59 +0530

    Update fetchFromAPI.js

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,27 +1,24 @@
-const axios = require('axios');
+// fetchFromAPI.js
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
 const options = {
-  method: 'GET',
-  url: 'https://youtube-v31.p.rapidapi.com/search',
   params: {
-    relatedToVideoId: '7ghhRHRP6t4',
-    part: 'id,snippet',
-    type: 'video',
-    maxResults: '50'
+    maxResults: 50,
   },
   headers: {
-    'x-rapidapi-key': '310a752742msh3f4175a84af1db4p18dc1djsn252089815ae7',
-    'x-rapidapi-host': 'youtube-v31.p.rapidapi.com'
-  }
+    'X-RapidAPI-Key':"cbe60a8ca8mshec41d9891e7078ap156edajsn4dcc5ba8e7f8",
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
 };
 
-async function fetchData() {
-	try {
-		const response = await axios.request(options);
-		console.log(response.data);
-	} catch (error) {
-		console.error(error);
-	}
-}
-
-fetchData();
+export const fetchFromAPI = async (url) => {
+  try {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+  } catch (error) {
+    console.error('API Error:', error.response ? error.response.data : error.message);
+    throw error;
+  }
+};
